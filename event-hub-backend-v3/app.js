327f633040dbbce73ec07d0fd6b1f6d7c75d5a60
Rushwan07 <rushdeveloper07@gmail.com>
2025-03-05 16:08:42 +0530

    Added

--- event-hub-backend-v3/app.js
+++ event-hub-backend-v3/app.js
@@ -20,6 +20,7 @@ app.use(express.json({ limit: "50mb" }));
 const allowedOrigins = [
   "http://localhost:5173",
   "https://main--extraordinary-fox-28d76d.netlify.app",
+  "https://eventhub-frontend-xi.vercel.app/"
 ];
 
 app.use(
